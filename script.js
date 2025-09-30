2c4442b115c8da372e9b24eebc866c61ce77cf5c
Ayan Ali <149358183+ayanali31990@users.noreply.github.com>
2025-09-30 12:14:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
-const getSumBtn = document.createElement("button");
+        const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+	let price = document.querySelectorAll('.price')
+	let sum = 0
+    for(let i=0;i<price.length;i++){
+        sum+=+price[i].innerText
+    }
+    
+let tr = document.createElement('tr')
+let td = document.createElement('td')
+td.innerText = sum	
+tr.append(td)
+let table = document.querySelector('table')
+table.append(tr)
+
 };
 
 getSumBtn.addEventListener("click", getSum);
-
